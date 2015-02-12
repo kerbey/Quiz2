9d0061d538581af54e84bc8d95c6e1d72fd90bcb
kerbey <kerbeychevalier@verizon.net>
2015-02-12 11:57:06 -0500

    Update jOptionClass.java

--- Authentification/src/jOptionClass.java
+++ Authentification/src/jOptionClass.java
@@ -5,7 +5,8 @@ public class jOptionClass
 	static String userName= "Kerbey", password="Chev";
 	public static void main(String[]args)
 	{
-		AuthentificationMethod.authentification(userName, password);
-		JOptionPane.showInputDialog("welcome "+userName);
+		AuthentificationClass.authentification(userName, password);
+		String newUserName=JOptionPane.showInputDialog(" end ");
+		JOptionPane.showInputDialog("thank you "+newUserName);
 	}
 }
