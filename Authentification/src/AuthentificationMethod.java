eadaaddd2591c7b3e8e4892fe1466a08b102bfe9
kerbey <kerbeychevalier@verizon.net>
2015-02-12 14:34:17 -0500

    Update AuthentificationMethod.java

--- Authentification/src/AuthentificationMethod.java
+++ Authentification/src/AuthentificationMethod.java
@@ -8,36 +8,36 @@ public class AuthentificationClass
 		String userName="kerbey", password="chev";
 		String newUserName=authentification(userName, password);
 		System.out.println(" end ");
-		System.out.println("thankyou "+newUserName);
+		//System.out.println("thankyou "+newUserName);block if wrong newUserName or wrong newPassword
 	}
 
 	public static String authentification(String userName, String password)
 	{
 		//blocked code used for AuthentificationMethod class. JOptionpane codes belong to JOptionpane class
-		Scanner input=new Scanner(System.in);
-		System.out.println("enter in your username");
-		//String newUserName=JOptionPane.showInputDialog("enter username");
-		 String newUserName=input.next();	
+		//Scanner input=new Scanner(System.in);
+		//System.out.println("enter in your username");
+		String newUserName=JOptionPane.showInputDialog("enter username");
+		// String newUserName=input.next();	
 		if (newUserName.contains(userName))
 		{
-			//String newPassword = JOptionPane.showInputDialog("enter password");
-			System.out.println("enter in your password");
-			String newPassword=input.next();
+			String newPassword = JOptionPane.showInputDialog("enter password");
+			//System.out.println("enter in your password");
+			//String newPassword=input.next();
 			if (newPassword.contains(password))
 			{
-				//JOptionPane.showMessageDialog(null, "welcome "+newUserName);
-				System.out.println("welcome "+newUserName);
+				JOptionPane.showMessageDialog(null, "welcome "+newUserName);
+				//System.out.println("welcome "+newUserName);
 			}
 			else 
 			{
-				//JOptionPane.showMessageDialog(null,"wrong password");
-				System.out.println("wrong password");
+				JOptionPane.showMessageDialog(null,"wrong password");
+				//System.out.println("wrong password");
 			}
 		}
 		else 
 		{
-			//JOptionPane.showMessageDialog(null,"wrong userName");
-			System.out.println("wrong userName");
+			JOptionPane.showMessageDialog(null,"wrong userName");
+			//System.out.println("wrong userName");
 		}
 		return newUserName;
 	}
